cc3020505eb3eabd803bd0ef8bd0247023e97778
PREM KUMAR VERMA <prem.btmtcs2232802@nfsu.ac.in>
2025-05-20 10:52:08 +0000

    #setting-up

--- campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
+++ campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
@@ -5,8 +5,7 @@ contract HelloGuardian {
 
     /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
-        // UNCOMMENT THE NEXT LINE TO MAKE THE TEST PASS
-        // return "Hello Guardian";
+        return "Hello Guardian";
     }
 
 }
